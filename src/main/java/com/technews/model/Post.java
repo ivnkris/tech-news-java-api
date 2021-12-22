1abbec3dba40b78d2a2ba22bedd9df5a575e5923
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-12-22 15:23:27 +0000

    added Comment entity model

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
-@JsonIgnoreProperties({"hybernateLazyInitializer", "handler"})
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "post")
 public class Post implements Serializable {
 
